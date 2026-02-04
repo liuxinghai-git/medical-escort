e68af5aeb0014109a13dcdedd9a7a6176c48547d
liuxinghai-git <77486599+liuxinghai-git@users.noreply.github.com>
2026-02-04 15:56:24 +0800

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    // ⬇️⬇️⬇️ 关键修改在这里 ⬇️⬇️⬇️
+    rollupOptions: {
+      external: [
+        '@supabase/supabase-js', // 告诉 Rollup 不要打包这个模块
+      ],
+    },
+    // ⬆️⬆️⬆️ 关键修改在这里 ⬆️⬆️⬆️
+  },
 })
